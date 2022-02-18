fd91635f378c5536ff10f00d6bddfb97ea976594
Sid <sidchopp@gmail.com>
2022-02-18 14:45:28 -0700

    auth added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
+import Authentication from './components/Authentication';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Hello World..
-        <h3>I am making some changes at the moment, so the App may be down for some time. Sorry for the inconvenience😞</h3>
-      </header>
+      <Header />
+      <Authentication />
     </div>
   );
 }
---
+++ src/components/Authentication.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Authenticator } from '@aws-amplify/ui-react';
+import { Heading, Button, Text, View, Image, useTheme, Link } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+
+
+// For Authentication UI
+const components = {
+
+  Header() {
+    const { tokens } = useTheme();
+    return (
+      <View textAlign="center" padding={tokens.space.large}>
+        <Image
+          alt="Amplify logo"
+          src="https://docs.amplify.aws/assets/logo-dark.svg"
+        />
+      </View>
+    );
+  },
+  Footer() {
+    const { tokens } = useTheme();
+    return (
+      <View textAlign="center" padding={tokens.space.large}>
+        <Text color={`${tokens.colors.neutral['80']}`}>
+          &copy;2022, All Rights Reserved.{' '}
+          <Link color="orange" fontWeight="bold" href="https://sid-projects.netlify.app/" target="_blank">
+            My Other Projects
+          </Link>
+        </Text>
+      </View>
+    );
+  },
+};
+
+function Authentication() {
+  return (
+    <Authenticator components={components} loginMechanisms={['username']}>
+      {({ signOut, user }) => (
+        <main >
+          <Heading level={4}>
+            Hello {' '}{user.username}
+          </Heading>
+          <Button size="small" onClick={signOut}>Sign out</Button>
+          {/* <Api /> */}
+        </main>
+      )}
+    </Authenticator>
+  )
+}
+
+export default Authentication;
